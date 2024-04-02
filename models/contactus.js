d11c087c7b28d2badcffe61094b1452939081309
Abdul Rehman <abrehman0346@gmail.com>
2024-04-02 16:31:41 +0500

    added missing subject in contactus

--- models/contactus.js
+++ models/contactus.js
@@ -3,10 +3,12 @@ const mongoose = require("mongoose")
 const schema = mongoose.Schema({
     name: String,
     email: String,
+    subject: String,
     message: String,
     datetime: String,
     isloggedIn: Boolean,
-    cus_id: String
+    cus_id: String,
+
 })
 
 const ContactUsModel = mongoose.model("contactus", schema)
