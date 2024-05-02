128c4b4659ecc7427dbeb999a2cf261e62449f0c
Abdul Rehman <abrehman0346@gmail.com>
2024-05-03 00:29:21 +0500

    upload

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 mongoose
-  .connect(localConnection, {
+  .connect(remoteConnection, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
